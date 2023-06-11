5fa073d19aa651c0883eeb2f9ee13ed9dd25dad3
torondatto <nguyentrongdat1108@gmail.com>
2023-06-11 19:15:45 +0700

    change local file

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import { fileURLToPath } from 'url';
 import bodyParser from "body-parser";
 // import expressLayouts from "express-ejs-layouts";
 import winston from "winston";
-import setRoutes from "./app/routes/index.route.js";
+import setRoutes from "./routes/index.route.js";
 
 // import {port, url} from "./app/configs/index.js";
 
-import {initDatabase, initLogging, initValidations, config} from "./app/configs/index.js";
+import {initDatabase, initLogging, initValidations, config} from "./configs/index.js";
 
 
 const app = express();
@@ -18,9 +18,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // console.log(__dirname)
 // app.use(expressLayouts)
-const viewsPath = path.join(__dirname, 'app', 'views');
+const viewsPath = path.join(__dirname, 'views');
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'app/views'));
+app.set('views', path.join(__dirname, 'views'));
 // app.use(cors)
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
